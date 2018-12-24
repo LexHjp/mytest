d11e65e77e9a9e2ee4d5d4614de0b8e0917dca0e
hjp <778209333@qq.com>
2018-12-24 21:07:33 +0800

    修改

--- demo_test/src/main/java/com/itheima/domain/test.java
+++ demo_test/src/main/java/com/itheima/domain/test.java
@@ -2,6 +2,7 @@ package com.itheima.domain;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("===============================");;
+        System.out.println("===============================");
+		System.out.println("===============222222================");
     }
 }
